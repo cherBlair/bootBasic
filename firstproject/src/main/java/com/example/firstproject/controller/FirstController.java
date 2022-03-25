d315eff403c064f5104264b5045aa21e208c2f10
Hana <64818459+Hana-Kimma@users.noreply.github.com>
2022-03-25 11:54:00 +0900

    start

---
+++ firstproject/src/main/java/com/example/firstproject/controller/FirstController.java
@@ -0,0 +1,15 @@
+package com.example.firstproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FirstController {
+
+    @GetMapping("/hi")
+    public String niceToMeetYou(Model model) {
+        model.addAttribute("username","야옹");
+        return "greetings"; //templates/greetings.mustache -> 브라우저로 전송!
+    }
+}
